030777b9ca28b2482260fd333e4cff40d89e1d0a
Satish Kumar Sharma <skwebserv@gmail.com>
2023-02-21 18:20:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import { Route, Routes } from 'react-router-dom'
+// import RootLayout from './components/RootLayout'
+// import { About, Contact, Home, User, PageNotFound } from './pages'
+
+
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route element={<RootLayout />}>
+//           <Route index element={<Home />} />
+//           <Route path='/contact' element={<Contact />} />
+//           <Route path='/about' element={<About />} />
+//           <Route path='/user'>
+//             <Route index element={<User />} />
+//           </Route>
+//         </Route>
+//         <Route path='*' element={<PageNotFound />} />
+//       </Routes>
+//     </>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
